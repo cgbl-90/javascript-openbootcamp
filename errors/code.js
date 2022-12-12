8cc78794c2e1513788d9fb9259ba74e10ce60f42
Carmen Lopez <carmenbaez2000@gmail.com>
2022-12-12 13:34:42 +0100

    Error management

---
+++ errors/code.js
@@ -0,0 +1,41 @@
+/*
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+};
+
+console.log()
+console.info()
+console.debug()
+console.warm()
+console.error()
+
+Error management library -> https://www.npmjs.com/package/winston
+
+*/
+
+const winston = require("winston");
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new winston.transports.File({ filename: "error.log", level: "error" }),
+    new winston.transports.File({ filename: "combined.log" }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
