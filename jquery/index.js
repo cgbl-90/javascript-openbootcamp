531377de3feab6923db62d6ce8178adeef7d0fe0
Carmen Lopez <carmenbaez2000@gmail.com>
2022-12-12 22:15:39 +0100

    JQuery

---
+++ jquery/index.js
@@ -0,0 +1,13 @@
+$("#name").css("color", "blue");
+
+$("#fname").css("font-size", "2em");
+$("#fname").css("color", "yellow");
+
+$("label").css("color", "red");
+
+/* https://www.freecodecamp.org/news/the-best-jquery-examples/ */
+
+$("button").click(() => {
+  // $("h1").show()
+  $("h1").fadeOut();
+});
