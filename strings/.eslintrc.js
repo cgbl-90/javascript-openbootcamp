f53bf0ac617435eeae440c4278a66a95caddc108
Carmen Lopez <carmenbaez2000@gmail.com>
2022-12-12 20:18:48 +0100

    Enlint

---
+++ strings/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "double"],
+  },
+};
+
+/* 
+All rules are here
+https://eslint.org/docs/latest/user-guide/configuring/rules */
